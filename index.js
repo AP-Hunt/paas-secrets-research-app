b7a60ac480008270e49be47043254ac6ed8fb5bd
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2020-10-24 16:26:59 +0100

    Talk to an API using an API key
    The API key must be set via an environment variable

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const Vault = require("node-vault");
 const express = require("express");
+const axios = require("axios")
 
-// const VCAP_APPLICATION = process.env["VCAP_APPLICATION"]
-// const vcapplicationJson = JSON.parse(VCAP_APPLICATION);
-// const APP_ID = vcapplicationJson["application_id"];
-// const APP_INSTANCE = process.env["CF_INSTANCE_INDEX"];
+const API_KEY = process.env["API_KEY"] || undefined;
 
 try
 {
-    // var vaultClient = Vault({
-    //     endpoint: 'http://localhost:9999'
-    // });
-
     const app = express();
     const port = process.env["PORT"];
 
     app.get("/", async (req, res) => {
-	res.setHeader("Content-Type", "application/json");
-        res.send(JSON.stringify(process.env, null, "\n"));
+        if(API_KEY === undefined) {
+            res.send(JSON.stringify(
+                {"error": "API_KEY environment variable is not set"},
+                null,
+                "\n"
+            ));
+            return;
+        }
+
+        axios.get(
+            "https://secrets-research-api.andyhunt.dev.cloudpipelineapps.digital",
+            {
+                auth: {
+                    username: "api_key",
+                    password: API_KEY
+                }
+            }
+        ).then(result => {
+            res.send(result.data);
+        }).catch(err => {
+            res.send(err);
+        })
     })
 
     app.listen(port, () => {
